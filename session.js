50019496705730c6edfa47d9cc3f306aa00c5f50
denethrasindu <rasindunimsara717@gmail.com>
2024-11-26 19:05:38 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "levanter_26cd19852800d5448ca5d2d09f013358e3",
 };
